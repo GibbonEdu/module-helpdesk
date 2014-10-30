7aa2cd854d96137709e03599f72e4aafac3ea3a0
raynichc <rclark@ichk.edu.hk>
2014-10-30 15:11:56 +0800

    Update issues_view.php

--- issues_view.php
+++ issues_view.php
@@ -23,19 +23,70 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 include "./modules/" . $_SESSION[$guid]["module"] . "/moduleFunctions.php" ;
 
 if (isModuleAccessible($guid, $connection2)==FALSE) {
-  //Acess denied
-  print "<div class='error'>" ;
-    print "You do not have access to this action." ;
-  print "</div>" ;
+	//Acess denied
+	print "<div class='error'>" ;
+		print "You do not have access to this action." ;
+	print "</div>" ;
 }
 else {
-  print "<h3>" ;
-  print _("Filter") ;
-  print "</h3>" ;
+	print "<h3>" ;
+	print _("Filter") ;
+	print "</h3>" ;
+
+	$and="" ;
+	$filter=NULL ;
+	if (isset($_GET["filter"])) {
+		$filter=$_GET["filter"] ;
+	}
+	else if (isset($_POST["filter"])) {
+		$filter=$_POST["filter"] ;
+	}
+	
+	$issueFilters = array("My Issues", "Resolved", "All");
+	
+	if ($filter=="") {
+		$filter=$issueFilters[0];
+		$and=" WHERE active='1'" ;
+	}
+	if ($filter=="Resolved") {
+		$and=" WHERE active='0'" ;
+	}
+
+	print "<form method='post' action='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=" . $_GET["q"] . "'>" ;
+		print"<table class='noIntBorder' cellspacing='0' style='width: 100%'>" ;	
+			?>
+			<tr>
+				<td> 
+					<b><?php print _('Issue Filter') ?></b><br/>
+					<span style="font-size: 90%"><i></i></span>
+				</td>
+				<td class="right">
+					<?php
+					print "<select name='filter' id='filter' style='width:302px'>" ;
+						
+						foreach($issueFilters as $option) {
+							$selected="" ;
+							if ($option==$filter) {
+								$selected="selected" ;
+							}
+							print "<option $selected value='" . $option . "'>". $option ."</option>" ;
+						}
+					print "</select>" ;
+					?>
+				</td>
+			</tr>
+			<?php
+			print "<tr>" ;
+				print "<td class='right' colspan=2>" ;
+					print "<input type='submit' value='" . _('Go') . "'>" ;
+				print "</td>" ;
+			print "</tr>" ;
+		print"</table>" ;
+	print "</form>" ;	
 
   try {
     $dataIssue=array("issueID"=>$row["issueID"]);
-    $sqlIssue="SELECT helpDeskIssue.* , surname , preferredName, gibbonPerson.title FROM helpDeskIssue JOIN gibbonPerson ON (helpDeskIssue.gibbonPersonID=gibbonPerson.gibbonPersonID)" ;
+    $sqlIssue="SELECT helpDeskIssue.* , surname , preferredName, gibbonPerson.title FROM helpDeskIssue JOIN gibbonPerson ON (helpDeskIssue.gibbonPersonID=gibbonPerson.gibbonPersonID)" . $and;
     $resultIssue=$connection2->prepare($sqlIssue);
     $resultIssue->execute($dataIssue);
   }
@@ -52,6 +103,7 @@ else {
       printf("<td>" .(($row['active'] == 1) ? "TRUE" : "FALSE"). "</td>");
       print "</tr>";
     }
-    print "</table>";
+    print "</table>";	
+
 }
 ?>
