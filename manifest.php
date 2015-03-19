f18abb25c70cc5277f685f3fe087d6df1fdace25
Ray Clark <rayclark@MBP095-Ray-Clark.local>
2015-03-19 15:22:28 +0800

    0.3.10 Update

--- manifest.php
+++ manifest.php
@@ -25,7 +25,7 @@ $description="A virtual Help Desk Moudle for Gibbon.";
 $entryURL="issues_view.php" ;
 $type="Additional" ;
 $category="Other" ;
-$version="0.4.00" ;
+$version="0.3.10" ;
 $author="Adrien Tremblay & Ray Clark" ;
 $url="https://github.com/raynichc/helpdesk" ;
 
--- version.php
+++ version.php
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information
  */
-$moduleVersion="0.4.00" ;
+$moduleVersion="0.3.10" ;
 
 /*
 */
