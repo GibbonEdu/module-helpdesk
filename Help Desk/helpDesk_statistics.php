768b37a807dc41dbc3df0e9bf09ddbb5d21f6113
Ray Clark <raynichc@users.noreply.github.com>
2020-08-28 14:40:30 +1000

    OOified Filter Form

--- Help Desk/helpDesk_statistics.php
+++ Help Desk/helpDesk_statistics.php
@@ -17,6 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Forms\Form;
+use Gibbon\Services\Format;
+
 @session_start() ;
 
 include './modules/'.$_SESSION[$guid]['module'].'/moduleFunctions.php';
@@ -34,15 +37,8 @@ if (isActionAccessible($guid, $connection2, "/modules/Help Desk/helpDesk_manageT
     print "</h3>" ;
 
     $d = new DateTime('first day of this month');
-    $startDate = dateConvertBack($guid, $d->format('Y-m-d')) ;
-    $endDate = dateConvertBack($guid, date("Y-m-d")) ;
-
-    if (isset($_POST["startDate"])) {
-        $startDate = $_POST["startDate"] ;
-    }
-    if (isset($_POST["endDate"])) {
-        $endDate = $_POST["endDate"] ;
-    }
+    $startDate = isset($_GET['startDate']) ? Format::dateConvert($_GET['startDate']) : $d->format('Y-m-d');
+    $endDate = isset($_GET['endDate']) ? Format::dateConvert($_GET['endDate']) : date("Y-m-d");
 
     $stats = array();
     $result = getLog($connection2, $_SESSION[$guid]["gibbonSchoolYearID"], getModuleIDFromName($connection2, "Help Desk"), null, null, $startDate, $endDate, null, null);
@@ -55,75 +51,28 @@ if (isActionAccessible($guid, $connection2, "/modules/Help Desk/helpDesk_manageT
         }
     }
     ksort($stats);
-    print "<form method='post' action='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=" . $_GET["q"] . "'>" ;
-        print"<table class='noIntBorder' cellspacing='0' style='width: 100%'>" ;
-            print "<tr>";
-                print "<td> ";
-                    print "<b>". __('Start Date Filter') ."</b><br/>";
-                    print "<span style=\"font-size: 90%\"><i></i></span>";
-                print "</td>";
-                print "<td class=\"right\">";
-                    print "<input name='startDate' id='startDate' maxlength=10 value='" . $startDate . "' type='text' style='height: 22px; width:100px; margin-right: 0px; float: none'></input>" ;
-                    print "<script type=\"text/javascript\">" ;
-                        print "var ttDate1=new LiveValidation('startDate');" ;
-                        print "ttDate1.add( Validate.Format, {pattern:" ;
-                            if ($_SESSION[$guid]["i18n"]["dateFormatRegEx"] == "") {
-                                print "/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/i" ;
-                            } else { 
-                                print $_SESSION[$guid]["i18n"]["dateFormatRegEx"] ;
-                            } 
-                            print ", failureMessage: \"Use " ;
-                            if ($_SESSION[$guid]["i18n"]["dateFormat"] == "") {
-                                print "dd/mm/yyyy" ;
-                            } else { 
-                                print $_SESSION[$guid]["i18n"]["dateFormat"] ;
-                            } 
-                        print ".\" } );" ;
-                    print "</script>" ;
-                    print "<script type=\"text/javascript\">" ;
-                        print "$(function() {" ;
-                            print "$(\"#startDate\").datepicker();" ;
-                        print "});" ;
-                    print "</script>" ;
-                print "</td>";
-            print "</tr>";
-            print "<tr>";
-                print "<td> ";
-                    print "<b>".  __('End Date Filter') ."</b><br/>";
-                    print "<span style=\"font-size: 90%\"><i></i></span>";
-                print "</td>";
-                print "<td class=\"right\">";
-                    print "<input name='endDate' id='endDate' maxlength=10 value='" . $endDate . "' type='text' style='height: 22px; width:100px; margin-right: 0px; float: none'></input>" ;
-                    print "<script type=\"text/javascript\">" ;
-                        print "var ttDate2=new LiveValidation('endDate');" ;
-                        print "ttDate2.add( Validate.Format, {pattern:" ;
-                            if ($_SESSION[$guid]["i18n"]["dateFormatRegEx"] == "") {
-                                print "/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/i" ;
-                            } else { 
-                                print $_SESSION[$guid]["i18n"]["dateFormatRegEx"] ;
-                            } 
-                            print ", failureMessage: \"Use " ;
-                            if ($_SESSION[$guid]["i18n"]["dateFormat"] == "") {
-                                print "dd/mm/yyyy" ;
-                            } else { 
-                                print $_SESSION[$guid]["i18n"]["dateFormat"] ;
-                            } 
-                        print ".\" } );" ;
-                    print "</script>" ;
-                    print "<script type=\"text/javascript\">" ;
-                        print "$(function() {" ;
-                            print "$(\"#endDate\").datepicker();" ;
-                        print "});" ;
-                    print "</script>" ;
-                print "</td>";
-            print "</tr>";
-            print "<tr>" ;
-                print "<td class='right' colspan=2>" ;
-                    print "<input type='submit' value='" . __('Go') . "'>" ;
-                print "</td>" ;
-            print "</tr>" ;
-        print"</table>" ;
-    print "</form>" ;
+
+    $form = Form::create('helpDeskStatistics', $_SESSION[$guid]['absoluteURL'].'/index.php', 'get');
+
+    $form->addHiddenValue('q', '/modules/'.$_SESSION[$guid]['module'].'/helpdesk_statistics.php');
+
+    $row = $form->addRow();
+        $row->addLabel('startDate', __("Start Date Filter"));
+        $row->addDate('startDate')
+            ->setDateFromValue($startDate)
+            ->chainedTo('endDate');
+
+    $row = $form->addRow();
+        $row->addLabel('endDate', __("End Date Filter"));
+        $row->addDate('endDate')
+            ->setDateFromValue($endDate)
+            ->chainedFrom('startDate');
+
+    $row = $form->addRow();
+        $row->addFooter();
+        $row->addSubmit();
+
+    echo $form->getOutput();
 
     print "<h3>";
         print "Statistics" ;
