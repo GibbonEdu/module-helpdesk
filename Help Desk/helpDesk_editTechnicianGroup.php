b48a870a1229f7aaaeee221341838aec2467b1b4
Ray Clark <raynichc@users.noreply.github.com>
2020-09-03 11:27:31 +1000

    Removed unused import

--- Help Desk/helpDesk_editTechnicianGroup.php
+++ Help Desk/helpDesk_editTechnicianGroup.php
@@ -18,7 +18,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Forms\Form;
-use Gibbon\Forms\DatabaseFormFactory;
 use Gibbon\Module\HelpDesk\Domain\TechGroupGateway;
 
 $page->breadcrumbs
