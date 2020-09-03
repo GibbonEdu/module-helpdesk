0f714ad776dfcccb6f23b7d6059e864d170d2a90
Ray Clark <raynichc@users.noreply.github.com>
2020-09-03 15:08:59 +1000

    Fixed Formatting

--- Help Desk/issues_assign.php
+++ Help Desk/issues_assign.php
@@ -17,70 +17,60 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 use Gibbon\Forms\Form;
-@session_start() ;
 
-include './modules/'.$_SESSION[$guid]['module'].'/moduleFunctions.php';
+include './modules/' . $_SESSION[$guid]['module'] . '/moduleFunctions.php';
 
-if (isActionAccessible($guid, $connection2, "/modules/Help Desk/issues_view.php") == false) {
+if (!isActionAccessible($guid, $connection2, "/modules/Help Desk/issues_view.php")) {
     //Acess denied
     $page->addError(__('You do not have access to this action.'));
-    print "</div>" ;
 } else {
-    $issueID = null;
     if (isset($_GET["issueID"])) {
         $issueID = $_GET["issueID"];
-    } else {
+    
+        $isReassign = hasTechnicianAssigned($connection2, $issueID);
+
+        $title = $isReassign ? __('Reassign Issue') : __('Assign Issue');
+        $page->breadcrumbs->add($title);
+
+        $permission = $isReassign ? "reassignIssue" : "assignIssue";
+
+        if (getPermissionValue($connection2, $_SESSION[$guid]["gibbonPersonID"], $permission)) {
+            if (isset($_GET['return'])) {
+                returnProcess($guid, $_GET['return'], null, null);
+            }
+            
+            $form = Form::create('assignIssue',  $_SESSION[$guid]['absoluteURL'] . '/modules/' . $_SESSION[$guid]['module'] . '/issues_assignProcess.php?issueID=' . $issueID . '&permission=' . $permission, 'post');
+            $form->addHiddenValue('address', $_SESSION[$guid]['address']);
+            
+            $data = array("issueID" => $issueID);
+            
+            //TODO: Fix module_function getAllTechnicians to work here
+            $sql = "SELECT helpDeskTechnicians.gibbonPersonID, technicianID AS value, concat(surname, ', ', preferredName) AS name FROM helpDeskTechnicians JOIN gibbonPerson ON (helpDeskTechnicians.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE status='Full' ORDER BY surname, preferredName ASC";
+            
+            //Find curently assigned technician TODO: fix the getTechWorkingOnIssue function in module_functions so that it actually works and this can be replaced
+            $sqlvalues = "SELECT helpDeskTechnicians.gibbonPersonID AS personID, helpDeskTechnicians.technicianID AS value, concat(surname, ', ', preferredName) AS name FROM helpDeskIssue JOIN helpDeskTechnicians ON (helpDeskIssue.technicianID=helpDeskTechnicians.technicianID) JOIN gibbonPerson ON (helpDeskTechnicians.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonPerson.status='Full' AND issueID=:issueID ORDER BY name ASC";
+            $result = $connection2->prepare($sqlvalues);
+                $result->execute($data);
+                $values = $result->fetch();
+                
+            $row = $form->addRow();
+                $row->addLabel('technician', __('Technician'));
+                $row->addSelect('technician')
+                    ->fromQuery($pdo, $sql, $data)
+                    ->placeholder()
+                    ->isRequired()
+                    ->selected($values['value']); 
+            
+            $row = $form->addRow();
+                $row->addFooter();
+                $row->addSubmit();
+
+            echo $form->getOutput();
+        } else {
+            $page->addError(__('You do not have access to this action.'));
+        }
+    } else {    
         $page->addError(__('No issue selected.'));
-        exit();
     }
-
-    $isReassign = false;
-    if (hasTechnicianAssigned($connection2, $issueID)) {
-        $isReassign = true;
-    }
-
-    $permission = "assignIssue";
-
-    if ($isReassign) {
-        $permission = "reassignIssue";
-    }
-
-    if (!getPermissionValue($connection2, $_SESSION[$guid]["gibbonPersonID"], $permission)) {
-        $page->addError(__('You do not have access to this action.'));
-        exit();
-    }
-
-    //Proceed!
-    $title = $isReassign ? __('Reassign Issue') : __('Assign Issue');
-    $page->breadcrumbs->add($title);
-
-    if (isset($_GET['return'])) {
-        returnProcess($guid, $_GET['return'], null, null);
-    }
-    
-    
-    $form = Form::create('assignIssue',  $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module'].'/issues_assignProcess.php?issueID=' . $issueID . '&permission=' . $permission, 'post');
-    $form->addHiddenValue('address', $_SESSION[$guid]['address']);
-    
-    $data = array("issueID"=>$issueID);
-    
-    //TODO: Fix module_function getAllTechnicians to work here
-    $sql = "SELECT helpDeskTechnicians.gibbonPersonID, technicianID AS value, concat(surname, ', ', preferredName) AS name FROM helpDeskTechnicians JOIN gibbonPerson ON (helpDeskTechnicians.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE status='Full' ORDER BY surname, preferredName ASC";
-    
-    //Find curently assigned technician TODO: fix the getTechWorkingOnIssue function in module_functions so that it actually works and this can be replaced
-    $sqlvalues = "SELECT helpDeskTechnicians.gibbonPersonID AS personID, helpDeskTechnicians.technicianID AS value, concat(surname, ', ', preferredName) AS name FROM helpDeskIssue JOIN helpDeskTechnicians ON (helpDeskIssue.technicianID=helpDeskTechnicians.technicianID) JOIN gibbonPerson ON (helpDeskTechnicians.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonPerson.status='Full' AND issueID=:issueID ORDER BY name ASC";
-    $result = $connection2->prepare($sqlvalues);
-        $result->execute($data);
-        $values = $result->fetch();
-        
-    $row = $form->addRow();
-        $row->addLabel('technician', __('Technician'));
-        $row->addSelect('technician')->fromQuery($pdo, $sql, $data)->placeholder()->isRequired()->selected($values['value']); 
-    
-    $row = $form->addRow();
-        $row->addFooter();
-        $row->addSubmit();
-
-    echo $form->getOutput();
 }
 ?>
