ce7ad399636c547a21b411d0d981a3d25e8684b0
Ray Clark <raynichc@users.noreply.github.com>
2020-09-05 14:06:49 +1000

    Added Filter Rules

--- Help Desk/src/Domain/IssueGateway.php
+++ Help Desk/src/Domain/IssueGateway.php
@@ -31,11 +31,33 @@ class IssueGateway extends QueryableGateway
 
 
      public function queryIssues($criteria) {      
-     $query = $this
+        $query = $this
             ->newQuery()
             ->from('helpDeskIssue')
             ->cols(['helpDeskIssue.*', 'techID.gibbonPersonID AS techPersonID'])
             ->leftJoin('helpDeskTechnicians AS techID', 'helpDeskIssue.technicianID=techID.technicianID');
+
+        $criteria->addFilterRules([
+            'issue' => function($query, $status) {
+                //TODO: Figure this out
+                return $query;
+            },
+            'status' => function ($query, $status) {
+                return $query
+                    ->where('helpDeskIssue.status = :status')
+                    ->bindValue('status', $status);
+            },
+            'category' => function ($query, $category) {
+                return $query
+                    ->where('helpDeskIssue.category = :category')
+                    ->bindValue('category', $category);
+            },
+            'priority' => function ($query, $priority) {
+                return $query
+                    ->where('helpDeskIssue.priority = :priority')
+                    ->bindValue('priority', $priority);
+            },
+        ]);
 
        return $this->runQuery($query, $criteria);
     }
