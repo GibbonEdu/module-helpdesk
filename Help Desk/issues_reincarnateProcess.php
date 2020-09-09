b067ee5f78a75e8c27e8f0b9b46ba74a57749a76
Ashton Power <48048360+powertashton@users.noreply.github.com>
2020-09-09 19:17:13 +0800

    Revert "Fixes"
    This reverts commit f92321ee3b15c6d8cea11fbef331a4e8e2721b91.

--- Help Desk/issues_reincarnateProcess.php
+++ Help Desk/issues_reincarnateProcess.php
@@ -51,7 +51,7 @@ if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/issues_view.php
     $gibbonPersonID = $gibbon->session->get('gibbonPersonID');
 
     $techGroupGateway = $container->get(TechGroupGateway::class);
-    if (($issue['gibbonPersonID'] != $gibbonPersonID) && !($issueGateway->isRelated($issueID, $gibbonPersonID)) && $techGroupGateway->getPermissionValue($gibbonPersonID, 'reincarnateIssue')) {
+    if (($issue['gibbonPersonID'] != $gibbonPersonID) && !($issueGateway->isRelated($issueID, $gibbonPersonID) && $techGroupGateway->getPermissionValue($gibbonPersonID, 'reincarnateIssue')) {
         $URL .= '&return=error1';
         header("Location: {$URL}");
         exit();
