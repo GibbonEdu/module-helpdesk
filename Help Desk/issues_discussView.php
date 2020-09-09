b15f9f1564e6fe88b7e625c2770792e6c05ad24c
Ray Clark <raynichc@users.noreply.github.com>
2020-09-09 21:56:40 +1000

    Replaced formatting on privacy link

--- Help Desk/issues_discussView.php
+++ Help Desk/issues_discussView.php
@@ -139,7 +139,7 @@ if (!isModuleAccessible($guid, $connection2)) {
                     ->width($tdWidth)
                     ->format(function($row) use ($gibbon, $isPersonsIssue, $hasFullAccess) {
                         if ($isPersonsIssue || $hasFullAccess) {
-                            return '<a href="' . $gibbon->session->get('absoluteURL') . '/index.php?q=/modules/' . $gibbon->session->get('module') . '/issues_discussEdit.php&issueID='. $row['issueID'] . '">' .  __($row['privacySetting']) . '</a>';
+                            return Format::link('./index.php?q=/modules/' . $gibbon->session->get('module') . '/issues_discussEdit.php&issueID='. $row['issueID'], __($row['privacySetting']));
                         } else {
                             return __($row['privacySetting']);
                         }
