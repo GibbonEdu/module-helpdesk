4dc11e270edb4ba68dc47f1a4350a50ef97cac0d
Ray Clark <raynichc@users.noreply.github.com>
2020-09-11 11:05:20 +1000

    Removed duplicate techGroupGateway

--- Help Desk/issues_resolveProcess.php
+++ Help Desk/issues_resolveProcess.php
@@ -42,7 +42,6 @@ if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/issues_view.php
     $issueID = $_GET['issueID'] ?? '';
     
     $issueGateway = $container->get(IssueGateway::class);
-    $techGroupGateway = $container->get(techGroupGateway::class);
     $issue = $issueGateway->getByID($issueID);
 
     if (empty($issueID) || empty($issue)){
