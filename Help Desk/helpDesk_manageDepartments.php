28bc05fba6a855e43064d747ceea136104aff984
Ray Clark <raynichc@users.noreply.github.com>
2020-09-12 20:17:49 +1000

    Added Return

--- Help Desk/helpDesk_manageDepartments.php
+++ Help Desk/helpDesk_manageDepartments.php
@@ -27,6 +27,9 @@ if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/helpDesk_manage
     $page->addError(__('You do not have access to this action.'));
 } else {
     //Proceed!
+    if (isset($_GET['return'])) {
+        returnProcess($guid, $_GET['return'], null, null);
+    }
     
     $departmentGateway = $container->get(DepartmentGateway::class);
     $subcategoryGateway = $container->get(SubcategoryGateway::class);   
