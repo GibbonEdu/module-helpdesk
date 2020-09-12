ca18a5977d297d0a460b13ea1edefe43035902d9
Ray Clark <raynichc@users.noreply.github.com>
2020-09-12 16:48:29 +1000

    Added Edit Subcategory

--- Help Desk/helpDesk_editSubcategory.php
+++ Help Desk/helpDesk_editSubcategory.php
@@ -17,12 +17,45 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-$page->breadcrumbs->add(__('Edit a Subcategory'));
+use Gibbon\Forms\Form;
+use Gibbon\Module\HelpDesk\Domain\SubcategoryGateway;
 
 if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/helpDesk_manageDepartments.php')) {
     //Acess denied
     $page->addError(__('You do not have access to this action.'));
 } else {
+	$departmentID = $_GET['departmentID'] ?? '';
+    $subcategoryID = $_GET['subcategoryID'] ?? '';
 
+    $subcategoryGateway = $container->get(SubcategoryGateway::class);
+    $subcategory = $subcategoryGateway->getByID($subcategoryID);
+
+    if (empty($departmentID) || empty($subcategoryID) || empty($subcategory) || $subcategory['departmentID'] != $departmentID) {
+        $page->addError(__('Invalid Data Provided'));
+    } else {
+		$page->breadcrumbs
+			->add(__('Manage Departments'), 'helpDesk_manageDepartments.php')
+            ->add(__('Edit Department'), 'helpDesk_editDepartment.php', ['departmentID' => $departmentID])
+            ->add(__('Edit Subcategory'));
+
+     	$form = Form::create('createSubcategory',  $gibbon->session->get('absoluteURL') . '/modules/' . $gibbon->session->get('module') . '/helpDesk_editSubcategoryProcess.php', 'post');
+        $form->addHiddenValue('address', $gibbon->session->get('address'));
+        $form->addHiddenValue('departmentID', $departmentID);
+        $form->addHiddenValue('subcategoryID', $subcategoryID);
+
+        $row = $form->addRow();
+            $row->addLabel('subcategoryName', __('Subcategory Name'));
+            $row->addTextField('subcategoryName')
+                ->isRequired()
+                ->uniqueField('./modules/' . $gibbon->session->get('module') . '/helpDesk_createSubcategoryAjax.php', array('departmentID' => $departmentID, 'currentSubcategoryName' => $subcategory['subcategoryName']))
+                ->maxLength(55)
+                ->setValue($subcategory['subcategoryName']);
+
+        $row = $form->addRow();
+            $row->addFooter();
+            $row->addSubmit();
+
+        echo $form->getOutput();
+    }
 }
 ?>
--- Help Desk/helpDesk_editSubcategoryProcess.php
+++ Help Desk/helpDesk_editSubcategoryProcess.php
@@ -17,17 +17,72 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Module\HelpDesk\Domain\DepartmentGateway;
+use Gibbon\Module\HelpDesk\Domain\IssueGateway;
+use Gibbon\Module\HelpDesk\Domain\SubcategoryGateway;
+
 require_once '../../gibbon.php';
 
 require_once './moduleFunctions.php';
 
-$URL = $gibbon->session->get('absoluteURL') . '/index.php?q=/modules/' . $gibbon->session->get('module') . '/helpDesk_editSubcategory.php';
+$URL = $gibbon->session->get('absoluteURL') . '/index.php?q=/modules/' . $gibbon->session->get('module');
 
 if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/helpDesk_manageDepartments.php')) {
-    $URL .= '&return=error0';
+    $URL .= '/issues_view.php&return=error0';
     header("Location: {$URL}");
     exit();
 } else {
+    $departmentID = $_POST['departmentID'] ?? '';
+
+    $departmentGateway = $container->get(DepartmentGateway::class);
     
- }
+    if (empty($departmentID) || !$departmentGateway->exists($departmentID)) {
+        $URL .= '/helpDesk_manageDepartments.php&return=error1';
+        header("Location: {$URL}");
+        exit();
+    }
+
+    $URL .= "/helpDesk_editDepartment.php&departmentID=$departmentID";
+
+    $subcategoryID = $_POST['subcategoryID'] ?? '';
+
+    $subcategoryGateway = $container->get(SubcategoryGateway::class);
+    $subcategory = $subcategoryGateway->getByID($subcategoryID);
+
+    if (empty($subcategoryID) || empty($subcategory) || $subcategory['departmentID'] != $departmentID) {
+        $URL .= '&return=error1';
+        header("Location: {$URL}");
+        exit();
+    }
+
+    $subcategoryName = $_POST['subcategoryName'] ?? '';
+
+    if (empty($subcategoryName) || strlen($subcategoryName) > 55) {
+    	$URL .= '&return=error1';
+        header("Location: {$URL}");
+        exit();
+    }
+
+    $data = array('subcategoryName' => $subcategoryName, 'departmentID' => $departmentID);
+
+    if (!$subcategoryGateway->unique($data, ['subcategoryName', 'departmentID'], $subcategoryID)) {
+    	$URL .= '&return=error7';
+        header("Location: {$URL}");
+        exit();
+    }
+
+    unset($data['departmentID']);
+    if (!$subcategoryGateway->update($subcategoryID, $data)) {
+        $URL .= '&return=error2';
+        header("Location: {$URL}");
+        exit();
+    }
+
+    $gibbonModuleID = getModuleIDFromName($connection2, 'Help Desk');
+    setLog($connection2, $gibbon->session->get('gibbonSchoolYearID'), $gibbonModuleID, $gibbon->session->get('gibbonPersonID'), 'Subcategory Edited', array('subcategoryID' => $subcategoryID), null);
+
+    $URL .= '&return=success0';
+    header("Location: {$URL}");
+    exit();
+}
 ?>
