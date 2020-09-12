f5368dfb8894ec11298602774ca33c14f11b3680
Ray Clark <raynichc@users.noreply.github.com>
2020-09-12 16:07:16 +1000

    Added Create Subcategory

--- Help Desk/helpDesk_createSubcategory.php
+++ Help Desk/helpDesk_createSubcategory.php
@@ -17,12 +17,39 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-$page->breadcrumbs->add(__('Create a Subcategory'));
+use Gibbon\Forms\Form;
+use Gibbon\Module\HelpDesk\Domain\DepartmentGateway;
 
 if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/helpDesk_manageDepartments.php')) {
     //Acess denied
     $page->addError(__('You do not have access to this action.'));
 } else {
+    $departmentID = $_GET['departmentID'] ?? '';
 
+    $departmentGateway = $container->get(DepartmentGateway::class);
+    if (empty($departmentID) || !$departmentGateway->exists($departmentID)) {
+        $page->addError(__('No Department Selected.'));
+    } else {
+        $page->breadcrumbs
+            ->add(__('Edit Department'), 'helpDesk_editDepartment.php', ['departmentID' => $departmentID])
+            ->add(__('Create a Subcategory'));
+
+        $form = Form::create('createSubcategory',  $gibbon->session->get('absoluteURL') . '/modules/' . $gibbon->session->get('module') . '/helpDesk_createSubcategoryProcess.php', 'post');
+        $form->addHiddenValue('address', $gibbon->session->get('address'));
+        $form->addHiddenValue('departmentID', $departmentID);
+
+        $row = $form->addRow();
+            $row->addLabel('subcategoryName', __('Subcategory Name'));
+            $row->addTextField('subcategoryName')
+                ->isRequired()
+                ->uniqueField('./modules/' . $gibbon->session->get('module') . '/helpDesk_createSubcategoryAjax.php', array('departmentID' => $departmentID))
+                ->maxLength(55);
+
+        $row = $form->addRow();
+            $row->addFooter();
+            $row->addSubmit();
+
+        echo $form->getOutput();
+    }
 }
 ?>
