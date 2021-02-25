65f385bf3424c885bd6cd0b38de44483826ccbb4
Ray Clark <raynichc@users.noreply.github.com>
2021-02-25 17:06:39 +1100

    Incremented version

--- Help Desk/manifest.php
+++ Help Desk/manifest.php
@@ -23,7 +23,7 @@ $description="A virtual help desk module for Gibbon.";
 $entryURL="issues_view.php";
 $type="Additional";
 $category="Other";
-$version="1.4.20";
+$version="1.5.00";
 $author="Ray Clark, Ashton Power & Adrien Tremblay";
 $url="https://github.com/GibbonEdu/module-helpDesk";
 
