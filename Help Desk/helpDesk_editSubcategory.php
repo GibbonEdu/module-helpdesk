051c1bd1746d27043b28ec3de12dbe005798ae6c
Ray Clark <raynichc@users.noreply.github.com>
2021-06-01 17:40:22 +1000

    Implemented better checking

--- Help Desk/helpDesk_editSubcategory.php
+++ Help Desk/helpDesk_editSubcategory.php
@@ -18,6 +18,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Forms\Form;
+use Gibbon\Module\HelpDesk\Domain\DepartmentGateway;
 use Gibbon\Module\HelpDesk\Domain\SubcategoryGateway;
 
 if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/helpDesk_manageDepartments.php')) {
@@ -27,10 +28,12 @@ if (!isActionAccessible($guid, $connection2, '/modules/Help Desk/helpDesk_manage
     $departmentID = $_GET['departmentID'] ?? '';
     $subcategoryID = $_GET['subcategoryID'] ?? '';
 
+    $departmentGateway = $container->get(DepartmentGateway::class);
+
     $subcategoryGateway = $container->get(SubcategoryGateway::class);
     $subcategory = $subcategoryGateway->getByID($subcategoryID);
 
-    if (empty($departmentID) || empty($subcategoryID) || empty($subcategory) || $subcategory['departmentID'] != $departmentID) {
+    if (empty($departmentID) || !$departmentGateway->exists($departmentID) || empty($subcategory) || $subcategory['departmentID'] != $departmentID) {
         $page->addError(__('Invalid Data Provided'));
     } else {
         $form = Form::create('createSubcategory',  $gibbon->session->get('absoluteURL') . '/modules/' . $gibbon->session->get('module') . '/helpDesk_editSubcategoryProcess.php', 'post');
